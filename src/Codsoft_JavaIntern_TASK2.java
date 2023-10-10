2a41e8a9badbeb81d296d1480ca142062e0c9aad
ma <md.ali2001@hotmail.com>
2023-10-11 02:06:51 +0500

    .

---
+++ src/Codsoft_JavaIntern_TASK2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Codsoft_JavaIntern_TASK2 {
+
+    int subjects;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int subjects;
+        System.out.println("Enter number of subjects");
+        subjects = sc.nextInt();
+
+
+
+    }
+}
