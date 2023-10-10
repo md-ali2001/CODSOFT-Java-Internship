b869a5ecbb5a692db3ce86fec1e45f380de631e6
ma <md.ali2001@hotmail.com>
2023-10-11 01:59:13 +0500

    .

--- src/Codsoft_JavaIntern_TASK1.java
+++ src/Codsoft_JavaIntern_TASK1.java
@@ -6,30 +6,53 @@ public class Codsoft_JavaIntern_TASK1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int tries=25;
+
         int randomnumber = 1 + (int)(100
                 * Math.random());
 
+        System.out.println(
+              randomnumber);
+
         int userguess;
 
-        System.out.println(
-                "Guess the number:");
-
-
-        userguess = sc.nextInt();
-
-        if(userguess==randomnumber)
-        {
+        for(int i=1;i<=tries;i++) {
             System.out.println(
-                    "guess is right");
+                    "attempt : "+i+"   Guess the number:");
+
+
+            userguess = sc.nextInt();
+
+            if (userguess == randomnumber) {
+                System.out.println(
+                        "\nguess is right , you guessed it in "+i+" attempts");
+break;
+
+            }
+
+                else if(userguess<randomnumber)
+                {
+                    if((randomnumber-userguess)>30){
+                    System.out.println(
+                            "too low");}
+               else     if((randomnumber-userguess)>10){
+                        System.out.println(
+                                "low");}
+                }
+
+            else if(userguess>randomnumber)
+            {
+                if((userguess-randomnumber)>30){
+                    System.out.println(
+                            "too high");}
+             else   if((userguess-randomnumber)>10){
+                    System.out.println(
+                            "high");}
+            }
+
+
 
         }
-
-        else
-            System.out.println(
-                    "wrong guess");
-
-
-
 
 
 
