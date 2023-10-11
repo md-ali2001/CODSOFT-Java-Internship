cb01216789acecea7dc8774afb0565a1c8f173cb
ma <md.ali2001@hotmail.com>
2023-10-12 02:48:21 +0500

    .

--- src/Codsoft_JavaIntern_TASK2.java
+++ src/Codsoft_JavaIntern_TASK2.java
@@ -8,6 +8,43 @@ public class Codsoft_JavaIntern_TASK2 {
         int subjects;
         System.out.println("Enter number of subjects");
         subjects = sc.nextInt();
+        int totalmarks=subjects*100;
+        int marksobtained=0;
+        int subjectmarks;
+        String grade;
+
+        for(int i=1;i<=subjects;i++)
+        {
+            System.out.println("Enter marks of subject : "+i);
+            subjectmarks = sc.nextInt();
+            marksobtained=marksobtained+subjectmarks;
+
+
+
+        }
+
+        double percentage= (double) (marksobtained * 100) /totalmarks;
+        if(percentage>=80)
+         grade="A+";
+        else if(percentage>=70)
+            grade="A";
+        else if(percentage>=60)
+            grade="B";
+        else if(percentage>=50)
+            grade="D";
+        else
+            grade="FAIL";
+
+
+
+
+        System.out.println("\n***RESULT DETAILS***\n");
+        System.out.println("TOTAL SUBJECTS : "+subjects);
+        System.out.println("TOTAL MARKS : "+totalmarks);
+        System.out.println("OBTAINED MARKS : "+marksobtained);
+        System.out.println("PERCENTAGE : "+percentage);
+        System.out.println("GRADE : "+grade);
+
 
 
 
